41b938876d4ed51494c3c5ebaf7246b0e24ff425
piyushchoudhary2001 <57358377+piyushchoudhary2001@users.noreply.github.com>
2023-03-16 19:39:24 +0530

    Add files via upload

---
+++ Introduction/LearnSwing.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LearnSwing {
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.getContentPane().setBackground(Color.cyan);
+
+        frame.setLocation(200,200);
+        frame.setSize(800,600);
+        frame.setTitle("sachasdf");
+        frame.setBackground(Color.CYAN);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
